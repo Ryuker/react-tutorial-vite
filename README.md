788d5253a52686e6da47dbe77421a8e330699eb8
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-21 13:56:38 +0200

    added intro to app.tsx, updated readme with project specifics

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Vite React Tutorial portion of react-tutorial.app chapter 30
 uses node 16.14.2
 run nvm use 16
 run npm run dev
+
+project is using vite with typescript support. 
+runs on localhost:3000
+
--- react-tutorial-ch30-vite/src/App.tsx
+++ react-tutorial-ch30-vite/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <p>My First React App - Joeri</p>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
