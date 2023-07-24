a85fd68ec269508c5bc1e8b52bc80afdbe5606b7
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-24 16:17:46 +0200

    added Clock

---
+++ react-tutorial-ch30-vite/src/components/Clock.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+function Clock() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {                             // set timer interval on first render, each 1 sec it gets the new date
+    const intervalID = setInterval(() => {
+      setDate( new Date());
+    }, 1_000);
+
+    return () => { clearInterval(intervalID); } // clean up the interval when clock stops rendering to prevent memory leak.
+  }, []);
+
+  return 
+    <>
+      <h2>Clock: {date.toLocaleTimeString()}</h2>
+    </>
+}
+
+export default Clock;
