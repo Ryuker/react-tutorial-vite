58821d760319aecbbb19b485dcab5619f91b02bc
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-25 15:28:32 +0200

    added comment to explain what useEffect does

--- react-tutorial-ch30-vite/src/components/ThemeSwitcher.tsx
+++ react-tutorial-ch30-vite/src/components/ThemeSwitcher.tsx
@@ -1,24 +1,23 @@
 import {useState, useEffect} from 'react'
 
 function ThemeSwitcher() {
-  const [darkTheme, setDarkTheme] = useState(true)
+  const [darkTheme, setDarkTheme] = useState(true);
   const className = darkTheme ? "dark" : "light";
 
-  useEffect(() => {
+  useEffect(() => { // stores theme in local storage when darkTheme state changes
     localStorage.setItem("dark_theme", darkTheme.toString());
   }, [darkTheme]);
 
   function handleToggleClick(){
     setDarkTheme(!darkTheme);
   }
-
+  
 
   return (
     <>
       <h2>local storage widget</h2>
       <h3>Theme: {className}</h3>
       <button onClick={handleToggleClick}>toggle theme</button>
-
     </>
   )
 }
