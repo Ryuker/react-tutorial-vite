80bcc6a5ad3327350c7e235b2f1878da334a6511
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-26 17:53:00 +0200

    added interface to ensure types in FetchDataToState widget

--- react-tutorial-ch30-vite/src/components/FetchDataWidgets/FetchDataToState.tsx
+++ react-tutorial-ch30-vite/src/components/FetchDataWidgets/FetchDataToState.tsx
@@ -1,22 +1,31 @@
 import { useState, useEffect } from "react";
 
-function FetchDataToState(){
-  const [users, setUsers ] = useState();
+interface IUser {
+  id: string;
+  name: string;
+  username: string;
+}
 
-  // runs twice in development mode, fetches data after the component rendered.
+function FetchDataToState(){
+  const [users, setUsers ] = useState<IUser[]>();
+
+  // runs twice in development mode, fetches data after the component rendered, if there's no object in data yet.
   useEffect(() => {
-    fetch("https://react-tutorial-demo.firebaseio.com/users.json")
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setUsers(data);
-    });
-  }, []);
+    if (!users){
+      fetch("https://react-tutorial-demo.firebaseio.com/users.json")
+      .then(response => response.json())
+      .then((data:IUser[]) => {
+        console.log(data);
+        setUsers(data);
+      });
+    }
+  },[]);
 
   return(
     <>
       <h2>Fetch Data and store in state widget</h2>
-      <h3>{users}</h3>
+      <h3>{users?.map(user => `user: ${user.id} name: ${user.name} username: ${user.username}`)}</h3>
+      <h3>{JSON.stringify(users ?? "")}</h3>
     </>
   )
 }
