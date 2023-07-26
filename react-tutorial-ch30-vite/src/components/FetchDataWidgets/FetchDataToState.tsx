4fe9ddc1828078804b834e4f4daf09d07338072c
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-26 16:55:26 +0200

    added comment to FetchDataToState

--- react-tutorial-ch30-vite/src/components/FetchDataWidgets/FetchDataToState.tsx
+++ react-tutorial-ch30-vite/src/components/FetchDataWidgets/FetchDataToState.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function FetchDataToState(){
   const [users, setUsers ] = useState();
 
+  // runs twice in development mode, fetches data after the component rendered.
   useEffect(() => {
     fetch("https://react-tutorial-demo.firebaseio.com/users.json")
     .then(response => response.json())
