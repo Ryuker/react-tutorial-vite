921a3effe556a9a3569823c07a23e45c58d198b7
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-26 16:27:37 +0200

    added fetch data widget

---
+++ react-tutorial-ch30-vite/src/components/FetchDataWidget.tsx
@@ -0,0 +1,11 @@
+// URL: https://react-tutorial-demo.firebaseio.com/users.json
+
+function init(){
+  fetch("https://react-tutorial-demo.firebaseio.com/users.json")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
+}
+
+init();
