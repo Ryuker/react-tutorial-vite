c09c21ebb793304d9c4d97251dea7f52e6f4baeb
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-27 16:45:22 +0200

    added FetchDataDropdown widget

---
+++ react-tutorial-ch30-vite/src/components/FetchDataWidgets/FetchDataDropdown.tsx
@@ -0,0 +1,46 @@
+import { useState, ChangeEvent, useEffect } from "react";
+
+function FetchDataDropdown() {
+  const [currency, setCurrency] = useState<string>("");
+  const [rate, setRate] = useState<number>();
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  useEffect(() => {
+    if(currency){
+      setIsLoading(true);
+      fetch(`https://react-tutorial-demo.firebaseio.com/currencies/${currency}.json`)
+      .then(response => response.json())
+      .then((data:number) =>{
+        if(data)
+          setRate(data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    }
+      
+  }, [currency]);
+
+  function handleDropdownChange( event: ChangeEvent<HTMLSelectElement> ){
+    setCurrency(event.target.value);
+  }
+
+  return(
+    <>
+      <h2>Fetch Data Dropdown</h2>
+      <select onChange={handleDropdownChange} disabled={isLoading}>
+        <option value="">Select a currency</option>
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="cad">CAD</option>
+      </select>
+      <h3>1 {currency.toUpperCase()} = {rate} USD</h3>
+    </>
+  )
+}
+
+export default FetchDataDropdown;
