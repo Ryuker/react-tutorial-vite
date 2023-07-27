5f8002a4035c233e0d72ea84ce7f4e3909c3e4f5
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-27 18:17:24 +0200

    added PostDataWidget folder

---
+++ react-tutorial-ch30-vite/src/components/PostDataWidgets/PostData.tsx
@@ -0,0 +1,12 @@
+// for reference only
+fetch("https://react-tutorial-demo.firebaseio.com/grades.json", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({grade: 50})
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+});
