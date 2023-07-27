d4efb71f83872f738544f8249073da077c1d5020
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-27 17:22:26 +0200

    added FetchDataToStateAsyncButton widget

---
+++ react-tutorial-ch30-vite/src/components/FetchDataWidgets/FetchDataToStateAsyncButton.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Loader from "./Loader";
+
+interface IUser {
+  id: string;
+  name: string;
+  username: string;
+}
+
+function FetchDataToStateAsync(){
+  const [users, setUsers] = useState<IUser[]>();
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  async function handleLoadButtonClick(){
+    try{
+      setIsLoading(true);
+      const response = await fetch("https://react-tutorial-demo.firebaseio.com/users.json")
+      const data:IUser[] = await response.json();
+      setUsers(data);
+    }catch(error){
+      console.log(error);
+    }
+    finally{
+      setIsLoading(false);
+    }
+  }
+
+
+  return(
+    <>
+      <h2>Fetch Data to State with Button using Async Await</h2>
+      <button onClick={handleLoadButtonClick}>
+        {isLoading ? "Loading Users" : "Load Users"}
+        </button>
+      <h3>Users</h3>
+      {isLoading ? <Loader/> : 
+        <ul>
+          {users && users.map(user => <li>id: {user.id} name: {user.name} username: {user.username}</li>)}
+        </ul>
+      }
+    </>
+  )
+}
+
+export default FetchDataToStateAsync;
