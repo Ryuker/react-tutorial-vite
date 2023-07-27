ed397c79eccb325b10fc6d114ce03a45915c8042
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-27 18:18:29 +0200

    added comments to PostData

--- react-tutorial-ch30-vite/src/components/PostDataWidgets/PostData.tsx
+++ react-tutorial-ch30-vite/src/components/PostDataWidgets/PostData.tsx
@@ -1,10 +1,10 @@
 // for reference only
 fetch("https://react-tutorial-demo.firebaseio.com/grades.json", {
-    method: "POST",
+    method: "POST", // specified we are sending something
     headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json" // the format we're sending to the api
     },
-    body: JSON.stringify({grade: 50})
+    body: JSON.stringify({grade: 50})   // the data to add to the api
 })
 .then(response => response.json())
 .then(data => {
