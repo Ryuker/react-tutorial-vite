18feb81b4ea97e72ecc08466d78ee96cb217b3d3
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-28 18:57:21 +0200

    got useFetchHook working

--- react-tutorial-ch30-vite/src/components/UseFetchHook/FetchDataUsingHook.tsx
+++ react-tutorial-ch30-vite/src/components/UseFetchHook/FetchDataUsingHook.tsx
@@ -9,20 +9,31 @@ interface IUser {
 
 // doesn't work well yet
 function FetchDataUsingHook(){
+  let users: IUser[] = [] ;
   const {get} = useFetch("https://react-tutorial-demo.firebaseio.com/")
 
-
   const data = get("users.json");
-
-
   console.log(data);
+  
+  if (Array.isArray(data))
+    users = data.map((user:IUser) => {return user} )
+
+  console.log(users);
+
+  // const users = JSON.stringify(data);
+
+  // const users: Array<IUser> = data;
+
+  // const [{ name, id, username }] : { users: IUser[]} = data; 
   // const users = data.map((user:IUser) => {return {user}} )
 
   return (
     <>
       <h2>Fetch Data Using Custom Fetch Hook</h2>
       <h3>Display Data Here</h3>
-      {/* <p>{users}</p> */}
+      <ul>
+          {users && users.map(user => <li>id: {user.id} name: {user.name} username: {user.username}</li>)}
+        </ul>
     </>
   )
 
--- react-tutorial-ch30-vite/src/components/UseFetchHook/useFetch.hook.tsx
+++ react-tutorial-ch30-vite/src/components/UseFetchHook/useFetch.hook.tsx
@@ -1,14 +1,17 @@
+//https://www.typescriptlang.org/docs/handbook/2/generics.html < typescript generics explained
 
 export default function useFetch(baseUrl:string){
+
   async function get(url:string) {
     try{
       const response = await fetch(baseUrl + url);
       const data = response.json();
-      if (data)
+      if (data )
         return data;
     }catch(error){
       console.log(error);
     }
+    return null;
   }
 
   return {get};
