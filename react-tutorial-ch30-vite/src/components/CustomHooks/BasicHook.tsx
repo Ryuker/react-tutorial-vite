cdc36a88b521c2784f184b45a00e0ba518d5397c
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-28 14:58:13 +0200

    added once and repeat hooks

--- react-tutorial-ch30-vite/src/components/CustomHooks/BasicHook.tsx
+++ react-tutorial-ch30-vite/src/components/CustomHooks/BasicHook.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+
 export function useWelcomeGreeting(){
   console.log("Welcome to my greeting hook");
+}
+
+export function useWelcomeGreetingEffectOnce(){
+  useEffect(() => {
+    console.log("Welcome to my greeting hook");
+  }, [])
+}
+
+export function useWelcomeGreetingEffectEachRender(){
+  useEffect(() => {
+    console.log("Welcome to my greeting hook");
+  })
 }
