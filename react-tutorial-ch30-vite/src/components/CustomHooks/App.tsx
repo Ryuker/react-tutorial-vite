5ecbfc208d400af2337906236a8e35649103bd6b
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-28 14:53:54 +0200

    added very basic custom hook

---
+++ react-tutorial-ch30-vite/src/components/CustomHooks/App.tsx
@@ -0,0 +1,7 @@
+import { useWelcomeGreeting } from "./BasicHook";
+
+function App(){
+  useWelcomeGreeting(); // calling my custom hook
+}
+
+export default App;
---
+++ react-tutorial-ch30-vite/src/components/CustomHooks/BasicHook.tsx
@@ -0,0 +1,3 @@
+export function useWelcomeGreeting(){
+  console.log("Welcome to my greeting hook");
+}
