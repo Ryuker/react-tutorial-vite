674bd2e88122e7b0dfaada089576bee0d7cfaa48
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-30 16:46:38 +0200

    added loading render

--- react-tutorial-ch30-vite/src/components/UseFetchHook/postDataUsingHook.tsx
+++ react-tutorial-ch30-vite/src/components/UseFetchHook/postDataUsingHook.tsx
@@ -6,8 +6,13 @@ interface IUser {
   username: string;
 }
 
-function PostDataUsingHook(){
-  const {post} = useFetch("https://react-tutorial-demo.firebaseio.com/");
+function PostAndGetDataUsingHook(){
+  const {get, post, loading} = useFetch("https://react-tutorial-demo.firebaseio.com/");
+
+  function handleGet(){
+    get("users.json")
+    return;
+  }
 
   function handlePost(){
     const objectToPost:IUser = {id: "5", name: "Bob", username: "bobinator" };
@@ -18,8 +23,10 @@ function PostDataUsingHook(){
     <>
       <h2>Post Data Using Hook Widget</h2>
       <button onClick={handlePost}>post data</button>
+      <button onClick={handleGet}>get data</button>
+      <h2>{loading ? "loading...." : ""}</h2>
     </>
   )
 }
 
-export default PostDataUsingHook;
+export default PostAndGetDataUsingHook;
