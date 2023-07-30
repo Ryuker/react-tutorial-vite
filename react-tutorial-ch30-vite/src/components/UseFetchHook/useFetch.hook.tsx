16644fe2a5c32daa0d6ad4a1c9cfd9c46bb8116e
ryuker <lifeisagamenotaproduct@gmail.com>
2023-07-30 16:51:58 +0200

    added useFetch code

--- react-tutorial-ch30-vite/src/components/UseFetchHook/useFetch.hook.tsx
+++ react-tutorial-ch30-vite/src/components/UseFetchHook/useFetch.hook.tsx
@@ -1,21 +1,51 @@
-
+import { useState } from "react";
 
 export default function useFetch(baseUrl:string){
+  const[loading, setLoading] = useState<boolean>(true);
+
   function get(url:string){
-    return fetch(baseUrl + url)
+    return new Promise((resolve, reject) =>{
+      fetch(baseUrl + url)
       .then(response => response.json())
+      .then(data => {
+        if (!data) {
+          setLoading(false);
+          return reject(data);
+        }
+        setLoading(false);
+        resolve(data);
+      })
+      .catch(error => {
+        setLoading(false);
+        reject(error);
+      });
+    });
   }
 
   function post(url: string, body:object){
-    return fetch(baseUrl + url,{
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(body)
-    })
-      .then(response => response.json())
+    return new Promise((resolve, reject) => {
+      fetch(baseUrl + url, {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body)
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (!data) {
+            setLoading(false);
+            return reject(data);
+          }
+          setLoading(false);
+          resolve(data);
+        })
+        .catch(error => {
+          setLoading(false);
+          reject(error);
+        });
+    });
   }
 
-  return {get, post};
+  return {get, post, loading};
 }
